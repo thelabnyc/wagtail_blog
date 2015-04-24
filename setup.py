ae4a7f6ad41973d0700ae1b0c03704e012463856
David Burke <david@burkesoftware.com>
2015-04-24 12:45:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "wagtail-blog",
-    version = "0.3",
+    version = "0.4",
     author = "David Burke",
     author_email = "david@thelabnyc.com",
     description = ("A wordpress like blog app implemented in wagtail"),
