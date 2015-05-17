50631017caeea92fe77689404c95a0258e972c9f
Christina <christina@burkesoftware.com>
2015-05-17 00:37:33 +0000

    tags and categories

--- blog/management/commands/migrate_blog.py
+++ blog/management/commands/migrate_blog.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.db import IntegrityError
 import os
 import sys
 import json
 import requests
-from blog.models import BlogPage, BlogPageTag, BlogIndexPage, BlogCategory, BlogCategoryBlogPage
+from blog.models import BlogPage, BlogTag, BlogPageTag, BlogIndexPage, BlogCategory, BlogCategoryBlogPage
 from django.template.defaultfilters import slugify
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
@@ -34,45 +35,14 @@ class Command(BaseCommand):
             base_url = ''.join(('http://', args[0]))
         posts_url = ''.join((base_url,'/wp-json/posts'))
         tax_url = ''.join((base_url,'/wp-json/taxonomies'))
+        #import pdb; pdb.set_trace()
         try:
             fetched_posts = requests.get(posts_url)
         except ConnectionError:
             raise CommandError('There was a problem with the blog entry url.')
             pass
         posts = fetched_posts.json()
-
-
-        #still testing this
-        try:
-            fetched_tags_and_categories = requests.get(tax_url)
-        except ConnectionError:
-            raise CommandError('There was a problem with the taxonomy URL')
-
-        taxonomies = fetched_tags_and_categories.json()
-        """
-        for t in taxonomies:
-            if t['name'] == 'Categories':
-                name = t.get('name')
-                slug = t.get('slug')
-                parent_item = t.get('parent_item')
-                category = BlogCategory.objects.create(name=name, slug=slug, parent_item=parent_item)
-                category.save()
-                    if t['name'] == 'Tags':
-                        name = t.get('name')
-                        slug = t.get('slug')
-                        parent_item = t.get('parent_item')
-                        tag = BlogPageTag.objects.create(name=name, slug=slug, parent_item=parent_item)
-                        tag.save()
-        
-       """               
-       # new_category = blog_index.add_child(instance=BlogCategory(name="newcaadfasfsdftegory", slug="somekindofuniqueslug"))
-        new_category = BlogCategory.objects.create(name="slkjfsdkjlskdjflkdj", slug="slkjflskjdflksjdflksjdflkdsj")
-        
                
- 
-     
-        
-        
         #create BlogPage object for each record
         for post in posts:
             title = post.get('title')
@@ -84,6 +54,7 @@ class Command(BaseCommand):
             excerpt = post.get('excerpt')
             status = post.get('status')
             body = post.get('content')
+            featured_image = post.get('featured_image')
             #author/user data
             author = post.get('author')
             username = author['username']
@@ -94,30 +65,58 @@ class Command(BaseCommand):
             last_name = author['last_name']
             avatar = author['avatar']
             description = author['description']
-            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name)
+            try:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name)
+            except IntegrityError:
+                user = User.objects.get(username=username)
+
             date = post.get('date')[:10]
             date_modified = post.get('modified')
             
-            new_entry = blog_index.add_child(instance=BlogPage(title=title, slug=slug, search_description="description", date=date, url_path=url_path, depth=4, owner=user))
+            new_entry = blog_index.add_child(instance=BlogPage(title=title, slug=slug, search_description="description", date=date, owner=user))
+            #if there is a featured image, add it to the new entry
+            if featured_image:
+                new_entry.header_image = featured_image
+            #images - still working on this
+            
+            #for image_tag in re.findall("(<img\s[^>]*?src\s*=\s*['\"]([^'\"]*?)['\"][^>]*?>)", body):
+            #    image_tag_text = image_tag[0]
+            #    image_tag_src = image_tag[1]
+            #    filename = re.findall("/([^/]*)$", image_tag_src)[0]
+            #    urllib.urlretrieve(image_tag_src, os.path.join(settings.MEDIA_ROOT, "images", filename))
+            #    new_url = settings.MEDIA_URL + 'images/' + filename
+            #    body = body.replace(image_tag_src, new_url)            
+            
             #categories
-            categories = post.get('terms')
             categories_for_blog_entry = []
-            for c in categories.values():
-                category_name = c[0]['name']
-                category_slug = c[0]['slug']
-                new_category = BlogCategory.objects.create(name=category_name, slug=category_slug)   
-                categories_for_blog_entry.append(new_category)            
-
+            tags_for_blog_entry = []
+            categories = post.get('terms')
+            if len(categories) > 0: 
+                for record in categories.values():
+                    if record[0]['taxonomy'] == 'post_tag':
+                        tag_name = record[0]['name']
+                        tag_slug = record[0]['slug']
+                        new_tag = BlogPageTag.objects.get_or_create(name=tag_name, slug=tag_slug, content_object=new_entry)
+                        tags_for_blog_entry.append(new_tag)
+                    if record[0]['taxonomy'] == 'category':
+                        print('category')
+                        category_name = record[0]['name']
+                        category_slug = record[0]['slug']
+                        new_category = BlogCategory.objects.get_or_create(name=category_name, slug=category_slug)
+                        categories_for_blog_entry.append(new_category)
 
             #loop through categories_for_blog_entry and create BlogCategoryBlogPages(bcbp) for each category for this blog page
             bcbp = []
             for category in categories_for_blog_entry:
+                category = category[0]
                 connection = BlogCategoryBlogPage.objects.create(category=category, page=new_entry)
                 bcbp.append(connection)
             
             #save BlogCategoryBlogPage objects
-            for each in bcbp:
-                each.save()
+            for blog_category in bcbp:
+                blog_category.save()
+            for blog_tag in tags_for_blog_entry:
+                blog_tag.save()
             #save blog entry
             new_entry.save()       
-             
+            
