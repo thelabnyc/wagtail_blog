c7460cb40e67ee2eeda83ad7fc7d5eac40709841
Christina <christina@burkesoftware.com>
2015-05-26 15:35:42 +0000

    added password hashers from django-hashers-passlib to settings file

--- demo/settings.py
+++ demo/settings.py
@@ -119,3 +119,13 @@ COMPRESS_PRECOMPILERS = (
     ('text/x-scss', 'django_libsass.SassCompiler'),
 )
 
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+    'hashers_passlib.phpass',
+)
