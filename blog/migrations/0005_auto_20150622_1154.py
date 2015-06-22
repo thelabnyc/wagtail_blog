547725253d5b7fdb917ada652eae64922d29afbc
David Burke <dmbst32@gmail.com>
2015-06-22 11:57:29 -0400

    Fixed bad migration

--- blog/migrations/0005_auto_20150622_1154.py
+++ blog/migrations/0005_auto_20150622_1154.py
@@ -9,6 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('taggit', '0001_initial'),
+        ('blog', '0004_auto_20150427_2047'),
     ]
 
     operations = [
@@ -29,11 +30,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='blogcategory',
             name='parent',
-            field=models.ForeignKey(blank=True, to='blog.BlogCategory', help_text='Categories, unlike tags, can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.', related_name='children', null=True),
+            field=models.ForeignKey(blank=True, related_name='children', null=True, to='blog.BlogCategory', help_text='Categories, unlike tags, can have a hierarchy. You might have a Jazz category, and under that have children categories for Bebop and Big Band. Totally optional.'),
         ),
         migrations.AlterField(
             model_name='blogpage',
             name='date',
-            field=models.DateField(verbose_name='Post date', default=datetime.datetime.today, help_text='This date may be displayed on the blog post. It is not used to schedule posts to go live at a later date.'),
+            field=models.DateField(default=datetime.datetime.today, verbose_name='Post date', help_text='This date may be displayed on the blog post. It is not used to schedule posts to go live at a later date.'),
         ),
     ]
