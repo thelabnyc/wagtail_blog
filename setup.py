7f20be4d76ff4b06dcd197a8f0fec0655deca4cc
David Burke <dmbst32@gmail.com>
2015-07-06 11:21:18 -0400

    New version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "wagtail-blog",
-    version = "1.4.5",
+    version = "1.4.6",
     author = "David Burke",
     author_email = "david@thelabnyc.com",
     description = ("A wordpress like blog app implemented in wagtail"),
