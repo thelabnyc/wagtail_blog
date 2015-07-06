39d7a466f7cf01cd1bb8be1de8a3e3b88602d6eb
Christina Levengood <fromageball@gmail.com>
2015-07-06 13:41:37 -0400

    created a test blog page for test_blog_categories_and_tags

--- blog/tests.py
+++ blog/tests.py
@@ -13,14 +13,24 @@ class BlogTests(TestCase):
         self.user = User.objects.create_user('test', 'test@test.test', 'pass')
         self.blog_index = home.add_child(instance=BlogIndexPage(
             title='Blog Index', slug='blog', search_description="x",
-            owner=self.user
-        ))
+            owner=self.user))
+        self.blog_page = self.blog_index.add_child(instance=BlogPage(title="blog entry", 
+                         slug="blog-entry", search_description="description", body="some content goes here",
+                         owner=self.user, date='10:10:2010'))
         self.parent_blog_category = BlogCategory.objects.create(name="first category", slug="first-category")
         self.child_blog_category = BlogCategory.objects.create(name="second category", slug="second-category", parent=self.parent_blog_category)
         self.first_tag = BlogTag.objects.create(name="first tag", slug="first-tag")
         self.second_tag = BlogTag.objects.create(name="second tag", slug="second-tag")
 
     def test_import(self):
+        """
+        Tests migrate_wordpress command - 
+        	the command should do the following:
+        	1. create BlogPage objects from a given BlogIndex
+        	2. create category and tag objects as BlogCategory, 
+        	   BlogTag, BlogPageBlogCategory and BlogPageTag objects
+        The test imports from test-data.json which includes one wordpress blog post with 11 tags and 2 categories
+        """
         command = Command()
         with open('test-data.json') as test_json:
             posts = json.load(test_json)
@@ -46,13 +56,21 @@ class BlogTests(TestCase):
         
         
     def test_blog_categories_and_tags(self):
-        """Test to make sure that categories and tags are """
+        """Test to make sure that categories and tags are created correctly, 
+            and that a parent category is set correctly when it exists.
+            Tests to make sure that when there are categories and tags for a given blog post,
+            that BlogPageTag and BlogPageBlogCategory objects are created for each combination of tags/categories and posts.
+            
+            This test does not test any of the imports from a wordpress site, it just tests
+            whether or not the objects are being created and uses 
+        
+        """
         self.assertEqual(BlogCategory.objects.all().count(), 2)
         blog_category_child = BlogCategory.objects.get(name="second category")
         blog_category_parent = BlogCategory.objects.get(name="first category")
         self.assertEqual(blog_category_child.parent, blog_category_parent)
         self.assertFalse(blog_category_child.parent, None)
-        page = BlogPage.objects.get()
+        page = self.blog_page
         page_category = BlogPageBlogCategory.objects.create(page=page, category=blog_category_child)
         second_category = BlogPageBlogCategory.objects.create(page=page, category=blog_category_parent)
         self.assertEqual(page_category.page, page)
