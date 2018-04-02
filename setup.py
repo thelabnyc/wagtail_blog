f929ec06e71d1850ef249ccc285c8f0a5dffc2e2
David Burke <david@burkesoftware.com>
2018-04-02 11:10:10 -0400

    New relaese 2.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "wagtail-blog",
-    version = "2.0.0",
+    version = "2.0.1",
     author = "David Burke",
     author_email = "david@thelabnyc.com",
     description = ("A wordpress like blog app implemented in wagtail"),
@@ -21,7 +21,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
     ],
     install_requires=[
-        'wagtail>=1.0.0',
+        'wagtail>=2.0.0',
         'requests',
         'lxml'
     ]
